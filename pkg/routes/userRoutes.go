19f21d2825e35bfd41c86bf1e58bd74fe3ac85a5
unknown <amirmohammadfiroozmehr77@gmail.com>
2022-06-18 12:14:18 +0430

    refine code

--- pkg/routes/userRoutes.go
+++ pkg/routes/userRoutes.go
@@ -3,7 +3,7 @@ package routes
 import (
 	"net/http"
 	"tiny-url/pkg/controllers"
-	"tiny-url/pkg/middlewares"
+	auth "tiny-url/pkg/middlewares"
 
 	"github.com/gorilla/mux"
 )
@@ -12,9 +12,9 @@ var userRoutes = func(router *mux.Router) {
 
 	router.HandleFunc("/google/login", controllers.UserGoogleLogin).Methods(http.MethodGet)
 	router.HandleFunc("/google/callback", controllers.GoogleCallBack).Methods(http.MethodGet)
-	router.HandleFunc("/create", middlewares.IsAdmin(controllers.CreateNewUser)).Methods(http.MethodPost)
-	router.HandleFunc("/showall", middlewares.IsAdmin(controllers.GetAllUsers)).Methods(http.MethodGet)
-	router.HandleFunc("/show", middlewares.IfJWTLoggedIn(controllers.GetUserById)).Methods(http.MethodGet)
-	router.HandleFunc("/expire/{urlId}", middlewares.IfJWTLoggedIn(controllers.SetURLExpired)).Methods(http.MethodPatch)
-	router.HandleFunc("/checkloggedin", middlewares.IfJWTLoggedIn(controllers.CheckLoggedIn)).Methods(http.MethodGet)
+	router.HandleFunc("/create", auth.IsAdmin(controllers.CreateNewUser)).Methods(http.MethodPost)
+	router.HandleFunc("/showall", auth.IsAdmin(controllers.GetAllUsers)).Methods(http.MethodGet)
+	router.HandleFunc("/show", auth.IfJWTLoggedIn(controllers.GetUserById)).Methods(http.MethodGet)
+	router.HandleFunc("/expire/{urlId}", auth.IfJWTLoggedIn(controllers.SetURLExpired)).Methods(http.MethodPatch)
+	router.HandleFunc("/checkloggedin", auth.IfJWTLoggedIn(controllers.CheckLoggedIn)).Methods(http.MethodGet)
 }
